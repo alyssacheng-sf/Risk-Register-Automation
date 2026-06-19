ba8415ad97b9d7e16941ea9db940ac9d92e93dd2
Alyssa Cheng <alyssa.cheng@salesforce.com>
2026-06-19 10:48:17 -0700

    Delete risk-register-automation directory

--- risk-register-automation/src/__init__.py
+++
@@ -1,6 +0,0 @@
-# Risk Register Automation - MCE FY26
-from .gus_client import GUSClient
-from .risk_analyzer import RiskAnalyzer
-from .notification_builder import NotificationBuilder
-from .owner_mapper import OwnerMapper
-from .slack_client import SlackClient
